7165ad0778714246718caefec979762484750d68
ddurdle <ddurdle@gmail.com>
2015-05-26 11:49:10 -0400

    fix

--- console.pl
+++ console.pl
@@ -617,14 +617,13 @@ sub syncFolder($){
 				push(@subfolders, $resourceID);
   			 }else{
 
-    	print "bless = " . Scalar::Util::blessed($services[0]) . "\n";
 
   			#Google Drive (MD5 comparision) already exists; skip
   			if 	(Scalar::Util::blessed($dbase[$drives[0]]) eq 'pDrive::gDrive' and Scalar::Util::blessed($dbase[$drives[1]]) eq 'pDrive::gDrive'  and  defined($dbase[$drives[1]]{$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_md5'}].'_0'}) and  $dbase[$drives[1]]{$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_md5'}].'_0'} ne ''){
  				 print STDOUT "SKIP " . $$newDocuments{$resourceID}[pDrive::DBM->D->{'title'}] . "\n";
 
   			#already exists; skip
-  			}elsif 	(Scalar::Util::blessed($dbase[$drives[0]]) eq 'pDrive::gDrive' and Scalar::Util::blessed($dbase[$drives[1]]) eq 'pDrive::gDrive'  and  defined($dbase[$drives[1]]{$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_fisi'}].'_0'}) and  $dbase[$drives[1]]{$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_fisi'}].'_0'} ne ''){
+  			}elsif 	(defined($dbase[$drives[1]]{$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_fisi'}].'_0'}) and  $dbase[$drives[1]]{$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_fisi'}].'_0'} ne ''){
  				 print STDOUT "SKIP " . $$newDocuments{$resourceID}[pDrive::DBM->D->{'title'}] . "\n";
 
   			}else{

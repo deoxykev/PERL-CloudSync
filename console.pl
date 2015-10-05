c1b0d877417f3673021c4879851beb05e1a0f1c9
Dylan Durdle <ddurdle@gmail.com>
2015-10-05 12:20:46 -0400

    fix logging

--- console.pl
+++ console.pl
@@ -847,7 +847,7 @@ sub syncFolder($){
 
 							my $mypath = $services[$drives[$j]]->getFolderIDByPath($path, 1) if ($path ne '' and $path ne  '/');
 							print STDOUT  "upload to service ".($j+1)." ". $dbase[$drives[0]][0]{$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_fisi'}].'_'}."\n";
-					    	pDrive::masterLog('upload to service '.($j+1).' ('.$$newDocuments{$resourceID}[pDrive::DBM->D->{'title'}]. ', fisi '.$dbase[$drives[0]][0]{$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_fisi'}].'_'}.', md5 '.$dbase[$drives[0]][0]{$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_md5'}].'_'}."\n");
+					    	pDrive::masterLog('upload to service '.($j+1).' ('.$$newDocuments{$resourceID}[pDrive::DBM->D->{'title'}]. ', fisi '.$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_fisi'}].', md5 '.$$newDocuments{$resourceID}[pDrive::DBM->D->{'server_md5'}]."\n");
 							$services[$drives[$j]]->uploadFile( pDrive::Config->LOCAL_PATH.'/'.$$, $mypath, $$newDocuments{$resourceID}[pDrive::DBM->D->{'title'}]);
   						}
 					}

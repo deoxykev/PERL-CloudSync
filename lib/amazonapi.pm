e69730d75f7a02e7037590d8c82e2b1cc731c28d
Dylan Durdle <ddurdle@gmail.com>
2017-08-11 13:20:55 -0400

    testing

--- lib/amazonapi.pm
+++ lib/amazonapi.pm
@@ -489,6 +489,7 @@ sub downloadFile(*$$$){
   	FILE->autoflush;
   	binmode(FILE);
     my $res = $self->{_ua}->get($URL,':content_cb' => \&downloadChunk,':read_size_hint' => 8192,'Authorization' => 'Bearer '.$self->{_token});
+    print $res;
 	close(FILE);
   	print STDOUT "saved\n";
   	return;

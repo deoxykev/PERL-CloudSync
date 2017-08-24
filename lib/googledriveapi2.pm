f465a215bb2c1fd4af91a8fcfda36475c3441161
Dylan Durdle <ddurdle@gmail.com>
2017-08-24 14:38:51 -0400

    temp fix

--- lib/googledriveapi2.pm
+++ lib/googledriveapi2.pm
@@ -64,6 +64,32 @@ sub new() {
 }
 
 
+##
+# multiple NIC cards:
+# bind to a specific IP
+##
+sub bindIP(*$){
+
+	my $self = shift;
+  	my $IP = shift;
+
+  	$self->{_ua}->local_address($IP);
+
+}
+
+#
+# setTokens: access & refresh
+##
+sub setToken(*$$){
+	my $self = shift;
+	my $token = shift;
+	my $refreshToken = shift;
+
+	$self->{_refreshToken} = $refreshToken;
+	$self->{_token} = $token;
+
+}
+
 #
 # getTokens
 ##

6905dc43fe7000d71c5095fb25737342d2d002d0
Dylan Durdle <ddurdle@gmail.com>
2017-08-24 14:20:11 -0400

    move some routines up to cloudservice api

--- lib/cloudserviceapi.pm
+++ lib/cloudserviceapi.pm
@@ -1,4 +1,29 @@
 package pDrive::CloudServiceAPI;
 
+##
+# multiple NIC cards:
+# bind to a specific IP
+##
+sub bindIP(*$){
+
+	my $self = shift;
+  	my $IP = shift;
+
+  	$self->{_ua}->local_address($IP);
+
+}
+
+#
+# setTokens: access & refresh
+##
+sub setToken(*$$){
+	my $self = shift;
+	my $token = shift;
+	my $refreshToken = shift;
+
+	$self->{_refreshToken} = $refreshToken;
+	$self->{_token} = $token;
+
+}
 
 1;
--- lib/googledriveapi2.pm
+++ lib/googledriveapi2.pm
@@ -64,33 +64,6 @@ sub new() {
 }
 
 
-
-##
-# multiple NIC cards:
-# bind to a specific IP
-##
-sub bindIP(*$){
-
-	my $self = shift;
-  	my $IP = shift;
-
-  	$self->{_ua}->local_address($IP);
-	$self->{_IP} = $IP;
-}
-
-#
-# setTokens: access & refresh
-##
-sub setToken(*$$){
-	my $self = shift;
-	my $token = shift;
-	my $refreshToken = shift;
-
-	$self->{_refreshToken} = $refreshToken;
-	$self->{_token} = $token;
-
-}
-
 #
 # getTokens
 ##

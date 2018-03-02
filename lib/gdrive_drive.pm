e5bd54610fe18d8d06034aeaa80b4388d8ad23f4
Dylan Durdle <ddurdle@gmail.com>
2018-03-01 21:54:42 -0500

    fix filenames with leading and trailing spaces

--- lib/gdrive_drive.pm
+++ lib/gdrive_drive.pm
@@ -270,6 +270,7 @@ sub getSubFolderID(*$$){
 	my $self = shift;
 	my $folderName = shift;
 	my $parentID = shift;
+	$folderName  =~ s/^\s+|\s+$//g;
 
 	my $URL = 'https://www.googleapis.com/drive/v2/files?includeTeamDriveItems=true&supportsTeamDrives=true&q=\''. $parentID.'\'+in+parents+and+trashed%3Dfalse&fields=nextLink%2Citems(kind%2Cid%2CmimeType%2Ctitle%2CfileSize%2CmodifiedDate%2CcreatedDate%2CdownloadUrl%2Cparents/parentLink%2Cmd5Checksum)';
 

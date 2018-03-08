98605de078bed5921d8cf59b235b65c798b563b2
Dylan Durdle <ddurdle@gmail.com>
2018-03-08 15:39:51 -0500

    testing

--- lib/gdrive_drive.pm
+++ lib/gdrive_drive.pm
@@ -317,6 +317,26 @@ sub getSubFolderIDList(*$$){
 
 }
 
+sub getAppFolderIDList(*$$){
+
+	my $self = shift;
+	my $folderName = shift;
+	my $URL = shift;
+
+	if ($URL eq ''){
+		$URL = 'https://www.googleapis.com/drive/v2/files?includeTeamDriveItems=true&supportsTeamDrives=true&spaces=appDataFolder&fields=nextLink%2Citems(kind%2Cid%2CmimeType%2Ctitle%2CfileSize%2CmodifiedDate%2CcreatedDate%2CdownloadUrl%2Cmd5Checksum%2Cparents/parentLink)';
+	}
+
+
+	my $driveListings = $self->{_serviceapi}->getList($URL);
+	$self->{_nextURL} =  $self->{_serviceapi}->getNextURL($driveListings);
+
+  	my $newDocuments = $self->{_serviceapi}->readDriveListings($driveListings);
+
+	return $newDocuments;
+
+}
+
 
 sub getSubFolderIDListWithMedia(*$$){
 
--- lib/googledriveapi2.pm
+++ lib/googledriveapi2.pm
@@ -462,23 +462,6 @@ sub getSubFolderIDList(*$$){
 #
 # get the folderID for a subfolder
 ##
-sub getAppFolderIDList(*$$){
-
-	my $self = shift;
-	my $URL = shift;
-	my $folderName = shift;
-
-	if ($URL eq ''){
-		$URL =  API_URL .'files?includeTeamDriveItems=true&supportsTeamDrives=true&spaces=appDataFolder&fields=nextLink%2Citems(kind%2Cid%2CmimeType%2Ctitle%2CfileSize%2CmodifiedDate%2CcreatedDate%2CdownloadUrl%2Cparents/parentLink%2Cmd5Checksum)';
-	}
-	#my $URL = 'https://www.googleapis.com/drive/v2/files?q=\''. $folderName.'\'+in+parents';
-
-	return $self->generalGETdata($URL);
-
-}
-#
-# get the folderID for a subfolder
-##
 sub getFolderList(*$$){
 
 	my $self = shift;
